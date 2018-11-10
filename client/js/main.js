1eef3f723afd4d2f5b5183b73a0bad4355bcb28c
Rafno <rafnar1993@gmail.com>
2018-11-10 13:18:50 +0000

    Reynt að bæta við að skila tile sem músin er á

--- client/js/main.js
+++ client/js/main.js
@@ -3,62 +3,59 @@
 // =============
 
 var g_images = {};
-
+/**
+ * Request preloads can be used to preload images, add all images to their correct file (client/img/*)
+ * then link the file here and add the name of the sprite.
+ */
 function requestPreloads() {
 
     var requiredImages = {
         grassTile   : './client/img/grassTile.png',
         grassTileHighlight  : './client/img/grassTileHighlight.png',
-        player_01   : './client/img/Player/player_01.png'
+        player   : './client/img/Player/player_01.png'
     };
 
     imagesPreload(requiredImages, g_images, preloadDone);
 }
 
 var g_sprites = {};
-
+/**
+ * Once finished, add the sprite as a new object of the g_sprites.
+ * Called via g_sprites.sprite
+ * kicks off by drawing an empty
+ */
 function preloadDone() {
 
     g_sprites.grassTile  = new Sprite(g_images.grassTile);
     g_sprites.grassTileHighlight = new Sprite(g_images.grassTileHighlight);
-    g_sprites.player_01  = new Sprite(g_images.player_01);
-
-    drawEmptyMap();
-    
+    g_sprites.player  = new Sprite(g_images.player);    
 }
 
 // Kick it off
 requestPreloads();
   // our canvas
-  function drawEmptyMap() {
-    for (let i = 0; i <= 640; i += 64) {
-      for (let j = 0; j <= 640; j += 64) {
-        g_sprites.grassTile.drawAt(g_ctx,i,j);
+/**
+ *  Draws our play area, grasstile I axis must be multiplied by 63, not 64
+ * @param {Tile} tile 
+ * @param {Socket.id} id 
+ */
+  function drawEmptyMapViaTiles(tile,id) {
+    for(let i = 0; i < tile.__tiles.length;i++){
+      for(let j = 0; j < tile.__tiles[i].length; j++){
+        g_sprites.grassTile.drawAt(g_ctx, i*63,j*64);
+        g_ctx.rect(i*63, j*64, 64, 64);
+        g_ctx.stroke();
       }
+      
     }
   };
 
-  function drawHighlight() {
-    var img = new Image();
-    img.onload = function () {
-      for (let i = 0; i <= 640; i += 64) {
-        for (let j = 0; j <= 640; j += 64) {
-          g_ctx.drawImage(img, i, j);
-        }
-      }
-      g_ctx.beginPath();
-      g_ctx.stroke();
-    };
-    img.src = './client/img/grassTileHighlight.png';
-  };
-  function drawPlayer(posX,posY) {
-    var img = new Image();
-    img.onload = function () {
-      g_ctx.drawImage(img, posX, posY);
-      g_ctx.beginPath();
-      g_ctx.stroke();
-    };
-    img.src = './client/img/Player/player_01.png';
-  };
+ function returnTileAt(posX, posY){
+  // This is our valid play area, outside of -28 and 610, we don't want to do anything. 
+  if((posX > -28 && posX < 610) && (posY > -28 && posX < 610)){
+      console.log("valid play area");
+   }
+   
+ }
 
  // draw();
