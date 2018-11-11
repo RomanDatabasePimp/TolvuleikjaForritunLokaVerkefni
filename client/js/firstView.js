d76da0ebf2d33d0172894476656a77d1e407d46b
helgigretar <helgigretargunnars.96@gmail.com>
2018-11-11 10:43:13 +0000

    firstview hverfur rétt

--- client/js/firstView.js
+++ client/js/firstView.js
@@ -1,52 +1,40 @@
 // Upphafstilla alla playera sem waiting
-let player1 = "Bob is waiting";
+let player1 = "Bob is ready to roll";
 let player2 = "Sara is waiting";
 let monster = "Monster is waiting";
 let playerOne;
 let playerTwo;
 let playerThree;
+let Container;
 // Kalla á fall sem upphafstillir playerana eftir því hvort þeir eru ready eða waiting. Tek inn booolean fylki
 let video;// er childid sem er video
 let main;// er parent sem er body
-let first = true;
 function allPlayerReady(data){
-  player1 = "Bob is waiting";
+  player1 = "Bob is ready to roll";
   player2 = "Sara is waiting";
   monster = "Monster is waiting";
   if(data.hasOwnProperty("hasnotgamestarted"))
   {  
+    
+    console.log("ég er að teikna playera")
     if(data.hasnotgamestarted[0] == true){player1 = "Bob is ready to roll";}
     if(data.hasnotgamestarted[1] == true){player2 = "Sara will kick your ass"}
     if(data.hasnotgamestarted[2] == true){monster= "The monster will eat all of you";}
-    // þarf aðs setja try og catch tiol þess að htmlid fyllist ekki 
-    try {
-      playerContainer.remove();
-    }catch{}
-    drawFirstView();
     drawPlayers();
-    video.remove();
-    first = true;
+    // þarf aðs setja try og catch tiol þess að htmlid fyllist ekki 
   }else{
-    try{video.remove()}catch{}
-      drawGameBackground();
-    // hér þarf að gera bakgrun sem er í gangi á meðan leikinum stendur.
+    document.getElementById('videoContainerinn').style.display = 'none';
+    console.log("Game has started");
+  }
 
-  }
+ // drawGameBackground();
+
 }
-function drawGameBackground() {
-  if(first == true){
-    
-      main = document.querySelector('body');
-      backVideo = appendChild(main, 'video', 'backgroundvideo');
-      backVideo.setAttribute('src','/client/videos/frontPic.mp4')
-      backVideo.setAttribute('autoplay','')
-      first = false;
-  }
-}
-function drawFirstView() {
-    /*  Get the location of main in HTML  */
+/*function drawFirstView() {
+    /*  Get the location of main in HTML  
     main = document.querySelector('body');
-    video = appendChild(main, 'video', 'myVideo');
+    Container = appendChild(main,'div','container');
+    video = appendChild(Container, 'video', 'myVideo');
     video.setAttribute('src','/client/videos/frontPic.mp4')
     video.setAttribute('autoplay','')
 }
@@ -62,24 +50,14 @@ function createElement(element, className = '') {
   }
   function addTextToNode(node, text) {
     node.appendChild(document.createTextNode(text));
-  }
+  }*/
   function drawPlayers(){
-    playerContainer = appendChild(main,'div','container');
-    playerOne = appendChild(playerContainer,'div','players');
-    playerOne.setAttribute('id','one');
-    addTextToNode(playerOne,player1);
-    playerTwo = appendChild(playerContainer,'div','players');
-    playerTwo.setAttribute('id','two');
-    addTextToNode(playerTwo,player2);
-    playerThree = appendChild(playerContainer,'div','players');
-    playerThree.setAttribute('id','three');
-    addTextToNode(playerThree,monster);
-  }
-  function playerReady(){
-    player1 = "Player one is ready";
-    playerOne.remove();
-    playerTwo.remove();
-    playerThree.remove();
-    drawPlayers();
+    console.log("drawing");
+    let first = document.getElementById('one');
+    first.innerHTML = player1;
+    let second = document.getElementById('two');
+    second.innerHTML = player2;
+    let third = document.getElementById('three');
+    third.innerHTML = monster;
   }
 
