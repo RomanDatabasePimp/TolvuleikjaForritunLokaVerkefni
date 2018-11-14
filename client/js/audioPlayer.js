163ba5776dee2e61c897aa7b5bdeacc6efaa2ab9
helgigretar <helgigretargunnars.96@gmail.com>
2018-11-14 17:20:10 +0000

    audio play komið bakgrunnur lagaður, game ending komið og animation hraðað

--- client/js/audioPlayer.js
+++ client/js/audioPlayer.js
@@ -1,7 +1,13 @@
 var victory = new Audio('./client/audios/victory.mp3');
 function victorySound(){
     document.getElementById('fireWorksContainerID').style.display = '';
+    document.getElementById("winnerNoteID").style.display='';
     console.log("kominn í winner");
     victory.play();
+    setTimeout(function(){ endWinner(); }, 8000);
     
+}
+function endWinner(){
+    document.getElementById('fireWorksContainerID').style.display = 'none';
+    document.getElementById("winnerNoteID").style.display = 'none';
 }
