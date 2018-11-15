e8429fdce00dd4a9344273b84d43fbc5096e6d68
Rafno <rafnar1993@gmail.com>
2018-11-15 02:33:13 +0000

    bætt við 50/50 sjéns á að teikna annað map

--- client/js/utils.js
+++ client/js/utils.js
@@ -22,6 +22,10 @@ function convertToMatrix(x) {
   return x * 64;
 }
 
+function randRange(min, max) {
+  return (min + Math.random() * (max - min));
+}
+
 function wrapRange (value, lowBound, highBound) {
   while (value < lowBound) {
 value += (highBound - lowBound);
